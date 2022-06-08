e98dcc3c070cf850c269e151b1d946597857dfc0
Sandi Sopian <shunasand73@gmail.com>
2022-06-08 17:11:28 +0700

    Recipe App done

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import "./App.css";
 import Axios from "axios";
 import { useState } from "react";
+import RecipeTile from "./RecipeTile";
 
 function App() {
   const [query, setQuery] = useState("");
   const [recipes, setRecipe] = useState([]);
+  const [healthLabels, setHealthLabels] = useState("vegan");
 
   const YOUR_APP_ID = process.env.REACT_APP_YOUR_APP_ID;
   const YOUR_APP_KEY = process.env.REACT_APP_YOUR_APP_KEY;
 
-  var url = `https://api.edamam.com/search?q=${query}&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&&health=alcohol-free`;
+  var url = `https://api.edamam.com/search?q=${query}&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&health=${healthLabels}`;
 
   async function getRecipes() {
     let result = await Axios.get(url);
-    setRecipe(result.data);
+    setRecipe(result.data.hits);
     console.log(result.data);
   }
 
@@ -28,9 +30,29 @@ function App() {
       <form className="app__searchForm" onSubmit={onSubmit}>
         <input type="text" className="app__input" placeholder="enter ingredients" value={query} onChange={(e) => setQuery(e.target.value)} />
         <input className="app__submit" type="submit" value="Search" />
+
+        <select className="app__healthLabels">
+          <option onClick={() => setHealthLabels("vegan")}>Vegan</option>
+          <option onClick={() => setHealthLabels("vegetarian")}>Vegetarian</option>
+          <option onClick={() => setHealthLabels("paleo")}>Paleo</option>
+          <option onClick={() => setHealthLabels("dairy-free")}>Dairy-free</option>
+          <option onClick={() => setHealthLabels("gluten-free")}>Gluten-free</option>
+          <option onClick={() => setHealthLabels("wheat-free")}>Wheat-free</option>
+          <option onClick={() => setHealthLabels("low-sugar")}>Low-sugar</option>
+          <option onClick={() => setHealthLabels("egg-free")}>Egg-free</option>
+          <option onClick={() => setHealthLabels("peanut-free")}>Peanut-free</option>
+          <option onClick={() => setHealthLabels("tree-nut-free")}>Tree-nut-free</option>
+          <option onClick={() => setHealthLabels("soy-free")}>Soy-free</option>
+          <option onClick={() => setHealthLabels("fish-free")}>Fish-free</option>
+          <option onClick={() => setHealthLabels("shellfish-free")}>Shellfish-free</option>
+        </select>
       </form>
 
-      <div></div>
+      <div className="app__recipes">
+        {recipes.map((recipe) => {
+          return <RecipeTile recipe={recipe} />;
+        })}
+      </div>
     </div>
   );
 }
--- src/RecipeTile.js
+++ src/RecipeTile.js
@@ -1,9 +1,13 @@
 import React from "react";
+import "./RecipeTile.css";
 
-export default function RecipeTile() {
+export default function RecipeTile({ recipe }) {
   return (
-    <div className="recipeTile">
-      <p>RecipeTile</p>
+    // recipe["recipe"]["image"].match(/\.(jpeg|jpg|gif|png)$/) != null && (
+    <div className="recipeTile" onClick={() => window.open(recipe["recipe"]["url"])}>
+      <img className="recipeTile__img" src={recipe["recipe"]["image"]} alt="" />
+      <p className="recipeTile__name">{recipe["recipe"]["label"]}</p>
     </div>
+    // )
   );
 }
